80a3d1cbb2047353dad350e67036558d9d29bd33
Piyush <piyushkvpoor@gmail.com>
2021-09-14 10:45:33 +0530

    connected all notes component on new note page so notes will get fetched at home & new note simultanously

--- src/components/CreateNote.js
+++ src/components/CreateNote.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react'
+import React, {useContext, useEffect} from 'react'
 import NewNoteForm from './NewNoteForm'
 import NoteItem from './NoteItem'
 import NoteContext from '../contextAPI/NotesContext/NoteContext'
@@ -6,14 +6,18 @@ import NoteContext from '../contextAPI/NotesContext/NoteContext'
 function Notes() {
 
     const fetchNote = useContext(NoteContext);
+    const {Note, getAll} = fetchNote
 
+    useEffect(() => {
+        getAll()
+    }, [])
 
     return (
         <div className = "container my-3">
             <NewNoteForm/>
             <h3 className="my-4">Your recent notes: </h3>
             <div className="row">
-            {fetchNote.Note.map((eachNote)=> 
+            {Note.map((eachNote)=> 
                 <div className = "col-md-4"><NoteItem Title = {eachNote.Title} Description = {eachNote.Description} Tag = {eachNote.Tag} Key = {eachNote._id} /></div>
             )}
             </div>
